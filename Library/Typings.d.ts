29e08a3d05dc00b6f7d44c081b09fec94d213987
Simon Bächler <288516+sbaechler@users.noreply.github.com>
2025-09-29 21:25:46 +0200

    Update Library/Typings.d.ts
    Co-authored-by: MiroslavPetrik <miroslavpetrik@outlook.com>

--- Library/Typings.d.ts
+++ Library/Typings.d.ts
@@ -6,7 +6,7 @@ declare module 'accept-language' {
         /**
          * Define your supported languages. The first language will be your default language.
          */
-        languages(languages: readonly string[]): void;
+        languages(languages: readonly [string, ...string[]]): void;
 
         /**
          * Get matched language. If no match, the default language will be returned.
